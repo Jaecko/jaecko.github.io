e2d530ef39d1cf0dfb1455d4a4df8a519004730f
Jaecko <jackson-jamais203@hotmail.com>
2023-01-20 12:55:20 +0100

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+    $a = 3;
 
 ?>
 
@@ -11,17 +11,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
+    <link rel="stylesheet" href="src/css/app.css">
     <title>Test</title>
 </head>
 <body>
-    <!-- give me a card list to go to my differents github.io pages -->
-    <div class="card-list">
-        <div class="card">
-            <a href="https://github.io/">
-                <img src="
     
+    <h1><?= $a ?></h1>
+
+
+    <!-- Scripts -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
