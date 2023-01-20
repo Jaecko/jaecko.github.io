6691c1ee65b6bf6fa1661b032230350353a7e696
Jaecko <jackson-jamais203@hotmail.com>
2023-01-20 12:12:34 +0100

    php

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
+<?php 
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    
+
+
     <title>Test</title>
 </head>
 <body>
-    Hello world
+    <!-- give me a card list to go to my differents github.io pages -->
+    <div class="card-list">
+        <div class="card">
+            <a href="https://github.io/">
+                <img src="
+    
 </body>
 </html>
